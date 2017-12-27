9c39ab7a368605b4a00f3d25f55c22f728dbcc89
rami3l <33851577+rami3l@users.noreply.github.com>
2017-12-27 22:14:32 +0800

    Add files via upload

--- login_mod.sh
+++ login_mod.sh
@@ -6,17 +6,30 @@ retry=5
 
 #### DO NOT EDIT THIS LINE BELOW ####
 
-attemps=0
+attempts=0
 option=$1
+
+light_off()
+{
+#   The following line turns off the Internet LED of the Y1S router.
+    /bin/echo "0" > /sys/class/leds/y1s:blue:internet/brightness
+}
+
+light_on()
+{
+#   The following line turns on the Internet LED of the Y1S router.
+    /bin/echo "1" > /sys/class/leds/y1s:blue:internet/brightness
+}
 
 logout()
 {
     echo "Logging out..."
-    result=$(curl -s -k -A "$useragent" -d "action=logout&username=${username}&password=${password}&ajax=1" "https://10.200.21.4/include/auth_action.php")
+    result=$(curl -s -k -A "$useragent" -d "action=logout&username=${username}&password=${password}&ajax=1" "https://gw.buaa.edu.cn/include/auth_action.php")
     if [[ $result =~ "logout" ]]; then
         echo "Success"
     else
         echo $result
+    light_off
     fi
 }
 
@@ -28,13 +41,15 @@ urlencode()
 
 login()
 {
-    while [ $attemps -lt $retry ]
+    light_off
+    while [ $attempts -lt $retry ]
     do
-	    attemps=`expr $attemps + 1`
-	    echo "Sending login request... Attemp "$attemps
-	    result=$(curl -s -k -A "$useragent" -d "action=login&username=${username}&password={B}$(urlencode `echo -n $password|base64`)&ac_id=22&user_ip=&nas_ip=&user_mac=&save_me=1&ajax=1" "https://10.200.21.4:803/beihanglogin.php?ac_id=22&amp;url=https://10.200.21.4:803/beihangview.php")
+	    attempts=`expr $attempts + 1`
+	    echo "Sending login request... attempt "$attempts
+	    result=$(curl -s -k -A "$useragent" -d "action=login&username=${username}&password={B}$(urlencode `echo -n $password|base64`)&ac_id=22&user_ip=&nas_ip=&user_mac=&save_me=1&ajax=1" "https://gw.buaa.edu.cn:803/beihanglogin.php?ac_id=22&amp;url=https://gw.buaa.edu.cn:803/beihangview.php")
 	    if [[ $result =~ "login_ok" ]]; then
 		    echo "Login success! Your internet connection has been activated."
+            light_on
 #		    echo $(date)" Success" >> /tmp/login.txt
 		    break;
 	    else
@@ -47,7 +62,7 @@ login()
 
 get_info()  # TODO
 {
-    result=$(curl -s -k -A "$useragent" -d "action=get_online_info&key=45343" "https://10.200.21.4/include/auth_action.php")
+    result=$(curl -s -k -A "$useragent" -d "action=get_online_info&key=45343" "https://gw.buaa.edu.cn/include/auth_action.php")
 }
 
 main()
