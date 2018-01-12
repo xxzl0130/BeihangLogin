a11a58294f0cc8b33f98cb2a577a6ffbba39c444
rami3l <33851577+rami3l@users.noreply.github.com>
2018-01-12 17:38:05 +0800

    Add files via upload

---
+++ buaa_login.py
@@ -0,0 +1,84 @@
+import sys
+import requests as rq
+
+USERNAME = "Student ID No."
+PASSWORD = "Student ID PW"
+USER_AGENT = "Opera/9.23 (Nintendo Wii; U; ; 1038-58; Wii Internet Channel/1.0; en)"
+AUTH = rq.auth.HTTPBasicAuth(USERNAME, PASSWORD)
+RETRY = 5
+
+
+def light_off():
+    pass
+
+
+def light_on():
+    pass
+
+
+def logout():
+    print("Logging out...")
+    ua_dict = {'User-Agent': USER_AGENT}
+
+    data = [
+        ('action', 'logout'),
+        ('username', USERNAME),
+        ('password', PASSWORD),
+        ('ajax', '1'),
+    ]
+
+    post = rq.post("https://gw.buaa.edu.cn/include/auth_action.php",
+                   headers=ua_dict, data=data, verify=False)
+    return post
+
+
+def try_login():
+    ua_dict = {'User-Agent': USER_AGENT}
+
+    params = (
+        ('ac_id', '22'),
+        ('amp;url', 'https://gw.buaa.edu.cn:803/beihangview.php)'),
+    )
+
+    data = [
+        ('action', 'login'),
+        ('username', USERNAME),
+        ('password', PASSWORD),
+        ('ac_id', '22'),
+        ('user_ip', ''),
+        ('nas_ip', ''),
+        ('user_mac', ''),
+        ('save_me', '1'),
+        ('ajax', '1'),
+    ]
+
+    post = rq.post('https://gw.buaa.edu.cn:803/beihanglogin.php',
+                   headers=ua_dict, params=params, data=data, auth=AUTH, verify=False)
+    return post
+
+
+def login():
+    for i in range(RETRY):
+        print("Sending login request... attempt {}".format(i + 1))
+        response = try_login().text[19:27]
+        if response == "login_ok":
+            print("Login Success.")
+            break
+
+
+def get_info():
+    pass
+
+
+def main():
+    option = sys.argv[1]
+    if option == "login":
+        login()
+    elif option == "logout":
+        logout()
+    else:
+        print("Usage: \n login \n logout")
+
+
+if __name__ == "__main__":
+    main()
